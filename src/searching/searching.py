d5276369c9b77348be88adc6d7a768ead7d3e732
Tate Keller <tatek1993@gmail.com>
2020-07-20 19:35:33 -0500

    all tests passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -12,6 +12,22 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-    for i in range(0, len(arr)):
+    left, right = 0, len(arr) - 1
+
+    while len(arr) >= 1:
+
+        middle = int(left + right / 2)
+
+        # if the current node == target, return current node
+        if arr[middle] == target:
+            return middle
+
+        # if current node is less than target, set current node.right to current node
+        elif arr[middle] < target:
+            left, right = middle + 1, right
+
+        # else if current node is greater than target, set current node.left to current node
+        elif arr[middle] > target:
+            left, right = left, middle
 
     return -1  # not found
